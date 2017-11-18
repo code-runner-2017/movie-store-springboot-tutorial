8a148ea9ef263f096020cfe83024007b6cc1a366
code-runner-2017 <igliop@gmail.com>
2017-11-18 08:51:19 +0100

    .

--- src/main/java/com/moviestore/domain/Movie.java
+++ src/main/java/com/moviestore/domain/Movie.java
@@ -7,49 +7,49 @@ import javax.persistence.Id;
 
 @Entity
 public class Movie {
-	private String id;
-	private String title;
-	private String director;
-	private int year;
-	
-	public Movie() {
-		// default constructor is needed
-	}	
-	
-	public Movie(String title, String director, int year) {
-		super();
-		this.title = title;
-		this.director = director;
-		this.year = year;
-	}
+    private String id;
+    private String title;
+    private String director;
+    private int year;
+    
+    public Movie() {
+        // default constructor is needed
+    }    
+    
+    public Movie(String title, String director, int year) {
+        super();
+        this.title = title;
+        this.director = director;
+        this.year = year;
+    }
 
 
-	@Id
+    @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public void setDirector(String director) {
-		this.director = director;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	
-	
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    
 }
